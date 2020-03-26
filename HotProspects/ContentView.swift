d91769a91a455339f539ea5a4f04aadda784335a
reshetnikd <dmitry.reshetnik@icloud.com>
2020-03-26 19:23:13 +0200

    Try out some code with TabView and EnvironmentObject

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -8,9 +8,55 @@
 
 import SwiftUI
 
-struct ContentView: View {
+class User: ObservableObject {
+    @Published var name = "Taylor Swift"
+}
+
+struct EditView: View {
+    @EnvironmentObject var user: User
+    
     var body: some View {
-        Text("Hello, World!")
+        TextField("Name", text: $user.name)
+    }
+}
+
+struct DisplayView: View {
+    @EnvironmentObject var user: User
+    
+    var body: some View {
+        Text(user.name)
+    }
+}
+
+struct ContentView: View {
+    @State private var selectedTab = 0
+    
+    let user = User()
+    
+    var body: some View {
+//        VStack {
+//            EditView()
+//            DisplayView()
+//        }
+//        .environmentObject(user)
+        TabView(selection: $selectedTab) {
+            Text("Tab 1")
+                .onTapGesture {
+                    self.selectedTab = 1
+                }
+                .tabItem {
+                    Image(systemName: "star")
+                    Text("One")
+                }
+                .tag(0)
+            
+            Text("Tab 2")
+                .tabItem {
+                    Image(systemName: "star.fill")
+                    Text("Two")
+                }
+                .tag(1)
+        }
     }
 }
 
